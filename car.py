603d0c626d81906a31cd9a84c1a2c0c978011411
adeliya21 <adeliya.salieva@gmail.com>
2024-01-16 15:36:57 -0800

    add tire servicing criteria

--- car.py
+++ car.py
@@ -1,9 +1,10 @@
 from servisable import Servisable
 
 class Car(Servisable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tiers):
         self.engine = engine
         self.battery = battery
+        self.tires = tiers
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
